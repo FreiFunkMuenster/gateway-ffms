d84e8d25f0f9544ce904c6c3efc26921492a0363
Jan-Marten Brüggemann <fussel@fusselkater.org>
2014-07-31 14:31:15 +0200

    Path-Variablen in update.sh eingefügt, für nutzung mit cron

--- update.sh
+++ update.sh
@@ -5,6 +5,9 @@
 #   git clone https://github.com/FreiFunkMuenster/gateway-ffms
 #   chmod +x update.sh
 
+set -e
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 # Aktualisieren 
 cd /var/gateway-ffms
 git pull 
