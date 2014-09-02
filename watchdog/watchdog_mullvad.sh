7374357f6a8afece891a03dc69c0ea7be9e99669
Jan-Marten Brüggemann <fussel@fusselkater.org>
2014-09-03 00:27:49 +0200

    watchdog umgebaut, dass mehrere ips geprüft werden

--- watchdog/watchdog_mullvad.sh
+++ watchdog/watchdog_mullvad.sh
@@ -5,14 +5,11 @@
 # Set the number of the iptables mark, to catch the packets and route them through the tunnel
 iptables_mark=1
 
-# Set the destination ip to use for pinging
-destination_ip=8.8.8.8
+# Set the destination ips to use for pinging
+destination_ips=( '78.46.108.155' '78.46.108.150' '46.38.251.123' '8.8.8.8' )
 
 # Set the source interface
 source_if='tun0'
-
-# Set the maximum number of tries
-max_tries=3
 
 # Logfile path
 log_path='/var/log/watchdog_mullvad.log'
@@ -37,17 +34,16 @@ function log() {
 separator
 log "Trying to get a reply from $destination_ip..."
 
-while [ $max_tries -gt 0 ]; do
+for destination_ip in ${destination_ips[@]}; do
 	ping -q -I $source_if -c1 -m $iptables_mark $destination_ip >& /dev/null
 	if [ $? -eq 0 ]; then
 		log "Got a reply from $destination_ip, so everything seems to be in order. Exiting."
 		exit 0
 	fi
-	max_tries=`expr $max_tries - 1`
 	sleep 1
 done
 
-log "Did not get a reply from $destination_ip, so there seems to be a problem. Running 'execute_on_fail'..."
+log "Did not get a reply from any destination ip, so there seems to be a problem. Running 'execute_on_fail'..."
 execute_on_fail
 log "Exiting."
 exit 1
