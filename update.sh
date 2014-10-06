f38afe6e7f0093531ba2b990496bba8d1d8f884b
Christian Elberfeld <elberfeld@web.de>
2014-10-06 22:13:36 +0200

    erzeugen von node-namen aus update-script entfernt

--- update.sh
+++ update.sh
@@ -22,18 +22,20 @@ service bind9 reload
 service nagios-nrpe-server reload
 
 # Generate Bind DB with Node Names 
-python /var/gateway-ffms/nodenames.py > /var/tmp/db.nodes.ffms.new
-
-RETVAL=$?
-
-if [ $RETVAL -eq 0 ] 
-  then 
-  
-  # Copy generated output 
-  mv /var/tmp/db.nodes.ffms.new /var/tmp/db.nodes.ffms
-
-  # Reload Bind9
-  service bind9 reload
-
-fi 
+# Funktion auskommentiert - Dürfen alfred Daten eventuell nur beim Master gelesen werden ?? 
+#
+#python /var/gateway-ffms/nodenames.py > /var/tmp/db.nodes.ffms.new
+#
+#RETVAL=$?
+#
+#if [ $RETVAL -eq 0 ] 
+#  then 
+#  
+#  # Copy generated output 
+#  mv /var/tmp/db.nodes.ffms.new /var/tmp/db.nodes.ffms
+#
+#  # Reload Bind9
+#  service bind9 reload
+#
+#fi 
 
