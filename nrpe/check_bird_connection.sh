2c854ee23466d8f780666a12372c7816fa7ccb03
Christian Elberfeld <elberfeld@web.de>
2015-01-25 22:36:35 +0100

    sudo eingefügt

--- nrpe/check_bird_connection.sh
+++ nrpe/check_bird_connection.sh
@@ -3,7 +3,7 @@
 # Check der bird-Verbindung 
 # Die zu prüfende Verbindung wird als Paremeter übergeben
 
-output=$( birdc show protocols | grep $1 )
+output=$( sudo birdc show protocols | grep $1 )
 
 if [[ "#$output" =~ ^.*[m][a][s][t][e][r].*[u][p].*[E][s][t][a][b][l][i][s][h][e][d].*$ ]]
 then
