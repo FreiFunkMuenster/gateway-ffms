df39daafa2a698110c0fadfd1256c6352152eb2c
Christian Elberfeld <elberfeld@web.de>
2015-02-08 01:12:17 +0100

    check script für fastd connections

---
+++ nrpe/check_fastd_connections.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+# Check-Script um aus dem Fast-Status-Socket 
+# die Anzahl der aktiven Verbindungen auszulesen
+
+import socket
+import sys
+import json
+
+# Create a UDS socket
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = '/tmp/fastd-status'
+
+try:
+    # Open socket 
+	sock.connect(server_address)
+
+    received_count = 1
+    received_data = ""
+
+	# read data from socket 
+    while received_count > 0:
+        data = sock.recv(1024)
+        received_count = len(data)
+        received_data += data;
+
+	# close socket and parse json data 
+    sock.close();
+    received_json = json.loads(received_data);
+
+    connections = 0
+
+	# count connections 
+    for item in received_json.items()[2][1].items():
+        connection = item[1]
+        connections += 1
+        #print connection["name"]
+
+	# pront results 
+    print "Fastd Connections %d " % connections
+	
+    if connections < 2:
+        # return Critical
+        sys.exit(2)
+    elif connections < 50:
+        # return Warning
+        sys.exit(1)
+    else:
+      # return OK
+      sys.exit(0)
+
+except socket.error, msg:
+    print msg
+    sys.exit(2)
+
+except: 
+    print "Unknown Error"
+	sys.exit(2)
+
+	
