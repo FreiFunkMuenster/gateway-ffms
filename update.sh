fa11ac64ac2a036a1d93e992726b145cfd0f3f76
Christian Elberfeld <elberfeld@web.de>
2015-05-15 15:55:09 +0200

    erzeugung der nodenames entfernt

--- update.sh
+++ update.sh
@@ -19,18 +19,4 @@ kill -HUP $(pidof fastd)
 # Restart nrpe
 service nagios-nrpe-server reload
 
-# Generate Bind DB with Node Names 
-python /var/gateway-ffms/nodenames.py > /var/tmp/db.nodes.ffms.new
-RETVAL=$?
-
-if [ $RETVAL -eq 0 ] 
-  then 
-  
-  # Copy generated output 
-  mv /var/tmp/db.nodes.ffms.new /var/tmp/db.nodes.ffms
-
-  # Reload Bind9
-  service bind9 reload
-
-fi 
 
